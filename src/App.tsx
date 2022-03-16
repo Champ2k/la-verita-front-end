f28191bb8225ed76d664a542c04ede53b495728e
champ2k <wikrom.ch@ku.th>
2022-03-16 15:30:27 +0700

    prepare front end and test some input function and fetch data

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
+  type sentiment = {
+    negative: string;
+    neutral: string;
+    positive: string;
+  };
+  interface resultData {
+    inputword: string;
+    result: string;
+    sentiment: sentiment;
+  }
+
+  const [result, setResult] = useState<resultData>();
+  const [inputWord, setInputWord] = useState<string>("");
+
+  const getData = async (inputWord: string) => {
+    try {
+      const res = await axios.get(
+        `http://127.0.0.1:5000/analysis?inputword=${inputWord}`
+      );
+      return res.data;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const fetchData = async () => {
+    let response = await getData(inputWord);
+    setResult(response);
+  };
+
+  const handleInput = (e:any) => {
+    setInputWord(e.target.value)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        <input onChange={(e) => handleInput(e)}/>
+        <button type="button" onClick={() => fetchData()}>
+          Submit
+        </button>
+        <div>{result?.inputword}</div>
+        <div>{result?.result}</div>
+        <div>{result?.sentiment.negative}</div>
+        <div>{result?.sentiment.neutral}</div>
+        <div>{result?.sentiment.positive}</div>
+      </div>
+    </>
   );
 }
 
